772aff9e3068cf1533b305cd96bb808aba8563d1
wangsong <wangsong3635@outlook.com>
2015-07-16 13:17:03 +0800

    动态

--- mission.py
+++ mission.py
@@ -49,7 +49,7 @@ def mission_check(mission_name, mission_content, mission_starttime, mission_plan
 def mission_save(mission_name, mission_publisher, mission_content, mission_starttime, mission_plan_end_time, mission_duplicate):
 	#存储任务信息
 	mission_pubtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-	mission_status = "待审核"
+	mission_status = "已发布"
 
 
 	c = data.SQLconn()
@@ -85,15 +85,25 @@ def mission_list(account_name, role):
 	c = data.SQLconn()
 	conn = MySQLdb.connect(host=c["host"], user=c["user"], passwd=c["passwd"], charset=c["charset"], db=c["db"])
 	cursor = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
-
-	# cursor.execute("select account_id from account where account_username='%s';"%account_name)
-	# account_id = cursor.fetchall()[0]['account_id']
-
 	cursor.execute("select * from MISSION where %s='%s' and mission_status!='已关闭';"%(role, account_name))
 	m_list = cursor.fetchall()
 
 	conn.close()
 
+	m_list = list(m_list)
+	#按照starttime排序
+	m_list = sorted(m_list, key=lambda m_list: m_list['mission_starttime'])
+
+	return m_list
+def mission_list_type(account_name, role, mission_status):
+	#根据所需任务状态返回对应的list
+	c = data.SQLconn()
+	conn = MySQLdb.connect(host=c["host"], user=c["user"], passwd=c["passwd"], charset=c["charset"], db=c["db"])
+	cursor = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
+	cursor.execute("select * from MISSION where %s='%s' and mission_status = '%s';\
+		"%(role, account_name, mission_status.encode('utf-8')))
+	m_list = cursor.fetchall()
+	conn.close()
 	m_list = list(m_list)
 	#按照starttime排序
 	m_list = sorted(m_list, key=lambda m_list: m_list['mission_starttime'])
@@ -145,7 +155,7 @@ def mission_audit(mission_id):
 	conn = MySQLdb.connect(host=c["host"], user=c["user"], passwd=c["passwd"], charset=c["charset"], db=c["db"])
 	cursor = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
 	mission_id = int(mission_id)
-	cursor.execute("update MISSION set mission_status='已审核' where mission_id='%s'"%mission_id)
+	cursor.execute("update MISSION set mission_status='已审核' where mission_id='%s';"%mission_id)
 	conn.commit()
 	conn.close()
 
@@ -155,3 +165,10 @@ def get_mission_content(mission_id):
 	cursors = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
 	cursors.execute("SELECT mission_name, mission_content FROM mission WHERE mission_id = '%s'" % mission_id)
 	return list(cursors.fetchall())
+def mission_sta_change(mission_id ,mission_status):
+	c = data.SQLconn()
+	conn = MySQLdb.connect(host=c["host"], user=c["user"], passwd=c["passwd"], charset=c["charset"], db=c["db"])
+	cursor = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
+	cursor.execute("update mission set mission_status = %s where mission_id = '%s';"%(mission_status.encode('utf-8'), mission_id))
+	conn.commit()
+	conn.close()
